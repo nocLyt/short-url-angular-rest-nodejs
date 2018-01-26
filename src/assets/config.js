63842d195e4fbbe8fd26d7352d94df538be2f2d7
Li Tianyi <noclyt@gmail.com>
2018-01-26 06:10:42 -0500

    fix bug for asserts/config.js

--- src/assets/config.js
+++ src/assets/config.js
@@ -1,5 +1,5 @@
 
-export const PORT = 4096;
-export const PUBLIC_FILE_NAME = 'dist';
-export const INDEX_PAGE_RELATIVE_PATH = '/dist/index.html';
-export const DOMAIN = 'http://s.noclyt.com/'
+exports.PORT = 4096;
+exports.PUBLIC_FILE_NAME = 'dist';
+exports.INDEX_PAGE_RELATIVE_PATH = '/dist/index.html';
+
--- src/assets/url.js
+++ src/assets/url.js
@@ -1,4 +1,4 @@
-
-exports.URL_DOMAIN = "s.noclyt.com";
+exports.DOMAIN = 'http://s.noclyt.com/'
 exports.URL_API = "/api";
 exports.URL_API_DECODE = "/api/s/";
+
