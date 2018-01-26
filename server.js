1f3fdc48ff150df926b89a37fc89542662dab763
Li Tianyi <noclyt@gmail.com>
2018-01-26 03:27:20 -0500

    change port and add assert/config

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ var path = require('path');
 var bodyParser = require('body-parser');
 var index = require('./src/routers/main');
 var shortenURL = require('./src/routers/shorten-url');
+const port = require('./src/assets/config').PORT;
 
-const port = 8081;
 // 关于 fallback 用法
 // http://blog.lsqy.space/2017/03/28/170328%E5%8D%95%E9%A1%B5%E9%9D%A2%E5%BA%94%E7%94%A8%E7%9A%84History%E8%B7%AF%E7%94%B1%E6%A8%A1%E5%BC%8Fexpress%E5%90%8E%E7%AB%AF%E4%B8%AD%E9%97%B4%E4%BB%B6%E9%85%8D%E5%90%88/
 const fallback = require('express-history-api-fallback');
---
+++ src/assets/config.js
@@ -0,0 +1,3 @@
+
+
+exports.PORT = 4096;
