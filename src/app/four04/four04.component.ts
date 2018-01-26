53b819a883d4ff7d1b820411e7f663f9f5d8f1b8
Li Tianyi <noclyt@gmail.com>
2018-01-26 06:04:04 -0500

    fix bug

--- src/app/four04/four04.component.ts
+++ src/app/four04/four04.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
-import { DOMAIN } from '../../assets/config.js';
 
 @Component({
   selector: 'app-four04',
